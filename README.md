c82959465259e6f9fb84805264fd1cb55c9d282d
azureblue <kk0xf0@gmail.com>
2020-10-24 02:04:20 +0200

    updating everything

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # MindustryRPi
 ### [Mindustry](https://github.com/Anuken/Mindustry) for Raspberry Pi!
+#### ...and probably other armv7 or aarch64
 
 The JAR file for the Raspberry Pi is inside Assets in the Releases section: https://github.com/azureblue/MindustryRPi/releases
 
@@ -7,6 +8,7 @@ What is needed:
 - Raspberry Pi 4 (Rpi 3 not tested, but should work)
 - vc4 fake/full kms driver (dtoverlay=vc4-fkms-v3d or dtoverlay=vc4-kms-v3d in /boot/config.txt)
 - proper gpu_mem setting (tested with 128mb)
-- SDL (on rpi4: sudo apt install libsdl2-2.0 0)
+- SDL (sudo apt install libsdl2-2.0)
+- OpenAL (sudo apt install libopenal1, but probably you alrady have this one...)
 - java (sudo apt install openjdk-11-jre)
 - recommended (yet on your own responsibility :P) RPi overclocking ;) 
--- build-for-pi.sh
+++ build-for-pi.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-MINDUSTRY_RELEASE=$1
-ARC_RPI_COMMIT=$2
+ARC_RPI_COMMIT=$1
 BITS=${3:-32}
 LIB_SUFFIX=""
 if [[ $BITS = "64" ]]; then
     LIB_SUFFIX="64"
 fi
 
-wget https://github.com/azureblue/Arc/archive/$2.zip -O Arc-$2.zip  || exit -1
+if [ ! -f Mindustry.jar ]; then
+    echo "Missing Mindustry.jar!"
+    exit -1
+fi
 
-7z x Arc-$2.zip -y || exit -1
-cd Arc-$2
+wget https://github.com/azureblue/Arc/archive/$1.zip -O Arc-$1.zip  || exit -1
+
+7z x Arc-$1.zip -y || exit -1
+cd Arc-$1
 
 echo building Arc
 ./gradlew natives || exit -1
@@ -21,27 +25,21 @@ echo building Arc
 cd ..
 
 mkdir Mindustry
+cp Mindustry.jar Mindustry
 cd Mindustry
-wget https://github.com/Anuken/Mindustry/releases/download/$1/Mindustry.jar -O Mindustry.jar || exit -1
 
 7z x Mindustry.jar -y || exit -1
 rm *.dll *.so *.dylib
 rm -r win32-x86* linux-x86-64 darwin
 echo replacing sdl backend
 rm -r arc/backend/sdl || exit -1 
-cp -r ../Arc-$2/backends/backend-sdl/build/classes/java/main/arc/backend/sdl arc/backend || exit -1
-cp ../Arc-$2/backends/backend-sdl/libs/linuxarm$BITS/libsdl-arcarm$LIB_SUFFIX.so . || exit -1
+cp -r ../Arc-$1/backends/backend-sdl/build/classes/java/main/arc/backend/sdl arc/backend || exit -1
+cp ../Arc-$1/backends/backend-sdl/libs/linuxarm$BITS/libsdl-arcarm$LIB_SUFFIX.so . || exit -1
 strip libsdl-arcarm$LIB_SUFFIX.so || exit -1
-cp ../Arc-$2/natives/natives-desktop/libs/libarcarm$LIB_SUFFIX.so . || exit -1
+cp ../Arc-$1/natives/natives-desktop/libs/libarcarm$LIB_SUFFIX.so . || exit -1
 strip libarcarm$LIB_SUFFIX.so || exit -1
-cp ../Arc-$2/natives/natives-freetype-desktop/libs/libarc-freetypearm$LIB_SUFFIX.so . || exit -1
+cp ../Arc-$1/natives/natives-freetype-desktop/libs/libarc-freetypearm$LIB_SUFFIX.so . || exit -1
 strip libarc-freetypearm$LIB_SUFFIX.so || exit -1
-
-wget https://raw.githubusercontent.com/Anuken/Mindustry/$1/core/src/mindustry/ui/dialogs/SettingsMenuDialog.java -O mindustry/ui/dialogs/SettingsMenuDialog.java || exit -1
-sed -i 's/graphics.checkPref("animatedwater",.*);/graphics.checkPref("animatedwater", false);/g' mindustry/ui/dialogs/SettingsMenuDialog.java
-sed -i 's/graphics.checkPref("animatedshields",.*);/graphics.checkPref("animatedshields", false);/g' mindustry/ui/dialogs/SettingsMenuDialog.java
-sed -i 's/"bloom", true/"bloom", false/g' mindustry/ui/dialogs/SettingsMenuDialog.java
-javac  mindustry/ui/dialogs/SettingsMenuDialog.java
 
 rm Mindustry.jar
 
